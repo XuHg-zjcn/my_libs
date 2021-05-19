e03aa58ba13a2458f80889a830ce967391b82caa
Xu Ruijun <1687701765@qq.com>
2021-05-19 19:17:36 +0800

    copy sender.hpp

---
+++ common/Inc/sender.hpp
@@ -0,0 +1,23 @@
+/*
+ * sender.hpp
+ *
+ *  Created on: 2021年5月11日
+ *      Author: xrj
+ */
+
+#ifndef DATAFLOW_INC_SENDER_HPP_
+#define DATAFLOW_INC_SENDER_HPP_
+
+#include "buffer.hpp"
+#include "cmsis_os2.h"
+#include "usbd_customhid.h"
+
+typedef struct{
+	Buffer *buf;
+	USBD_HandleTypeDef *pdev;
+	u32 n;
+}SenderParam;
+
+void UsbSender_ProcFunc(void *argument);
+
+#endif /* DATAFLOW_INC_SENDER_HPP_ */
