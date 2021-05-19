f9dde8756725e827b053482b22e8cdce9450104d
Xu Ruijun <1687701765@qq.com>
2021-05-19 16:17:13 +0800

    feat: x_logs.cpp/hpp

---
+++ common/Inc/x_logs.hpp
@@ -0,0 +1,26 @@
+/*
+ * x_logs.h
+ *
+ *  Created on: May 19, 2021
+ *      Author: xrj
+ */
+
+#ifndef COMMON_INC_X_LOGS_HPP_
+#define COMMON_INC_X_LOGS_HPP_
+
+#include <string>
+using namespace std;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void X_Error(string file, int line);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* COMMON_INC_X_LOGS_HPP_ */
