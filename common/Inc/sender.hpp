bebffe9136253642837d2838ca3fc103430eec8a
Xu Ruijun <1687701765@qq.com>
2021-05-25 15:27:52 +0800

    feat: OLEDShow_N_TaskFunc

--- common/Inc/sender.hpp
+++ common/Inc/sender.hpp
@@ -20,11 +20,27 @@ typedef struct{
 	u32 n;
 }SenderParam;
 
+/*
+ * Show_fos only for `OLEDShow_ms_TimerFunc`,
+ * use `Interval_xx_Frames` in ssd1306.hpp as `ShowParam.fos_N_ms`
+ */
+typedef enum{
+	Show_Scroll,
+	Show_Fixed,
+	Show_fos
+}ShowType;
+
 typedef struct{
 	Buffer *buf;
 	SSD1306 *oled;
+	ShowType type;
+	uint32_t N_ms_fos;
+	uint8_t y0; //used for, 暂时不支持
+	uint8_t y1; //oled->commd_bytes(SET_PAGE_ADDR_2B, y0, y1);
 }ShowParam;
 
-void UsbSender_ProcFunc(void *argument);
+void UsbSender_TaskFunc(void *argument);
+void OLEDShow_N_TaskFunc(void *argument);
+void OLEDShow_ms_TimerFunc(void *argument);
 
 #endif /* DATAFLOW_INC_SENDER_HPP_ */
--- common/Src/sender.cpp
+++ common/Src/sender.cpp
@@ -9,7 +9,7 @@
 #include "buffhead.hpp"
 #include "main.h"
 
-void UsbSender_ProcFunc(void *argument)
+void UsbSender_TaskFunc(void *argument)
 {
 	SenderParam *param = (SenderParam*)argument;
 	BuffHeadReads *r_heads = &param->buf->r_heads;
@@ -22,4 +22,52 @@ void UsbSender_ProcFunc(void *argument)
 		USBD_CUSTOM_HID_SendReport(param->pdev, p, param->n*be);
 		HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
 	}
+}
+
+void OLEDShow_config(ShowParam *param)
+{
+	param->oled->commd_bytes(ADDRESSING_MODE_1B,  0x01);
+	param->oled->commd_bytes(V_SCORLL_AREA_2B, param->y0, param->y1);
+}
+
+void OLEDShow_N_TaskFunc(void *argument)
+{
+	ShowParam *param = (ShowParam*)argument;
+	OLEDShow_config(param);
+	uint32_t head_i = param->buf->r_heads.new_head();
+	uint16_t *p;
+	uint32_t n = param->N_ms_fos;
+	uint64_t column;
+	switch(param->type){
+	    case Show_Fixed:
+		    for(;;){
+			    p = (uint16_t*)param->buf->r_heads.get_frames(head_i, n*128);
+			    if(!p){
+					osThreadExit();
+			    }
+			    param->oled->commd_bytes(ADDRESSING_MODE_1B,  0x01);
+			    param->oled->commd_bytes(SET_COLUMN_ADDR_2B, 0, 127);
+			    param->oled->commd_bytes(SET_PAGE_ADDR_2B, 0, 7);
+			    for(uint32_t i=0;i<128;i++){
+					column = 0;
+					for(uint32_t j=0;j<n;j++){
+						column |= U64_BOTTOM>>(*p++/64);
+					}
+					param->oled->append_column(column);
+				}
+		    }
+			break;
+		case Show_Scroll:
+			break;//暂时没有实现
+	    case Show_fos:
+			break;//禁止使用
+	    default:
+			break;
+	}
+}
+
+void OLEDShow_ms_TimerFunc(void *argument)
+{
+	ShowParam *param = (ShowParam*)argument;
+	OLEDShow_config(param);
 }
