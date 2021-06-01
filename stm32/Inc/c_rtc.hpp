bbd799c2d79fe44ce86b362d6adb3ec0d441da3b
Xu Ruijun <1687701765@qq.com>
2021-06-01 19:45:25 +0800

    feat: class C_RTC

---
+++ stm32/Inc/c_rtc.hpp
@@ -0,0 +1,32 @@
+/*
+ * c_rtc.hpp
+ *
+ *  Created on: Jun 1, 2021
+ *      Author: xrj
+ */
+
+#ifndef STM32_INC_C_RTC_HPP_
+#define STM32_INC_C_RTC_HPP_
+
+#include <ctime>
+#include "mylibs_config.hpp"
+
+using namespace std;
+
+
+class C_RTC : public RTC_HandleTypeDef{
+public:
+	static void HAL2tm(struct tm* tm2, RTC_DateTypeDef *sDate, RTC_TimeTypeDef *sTime);
+	static void tm2HAL(struct tm* tm2, RTC_DateTypeDef *sDate, RTC_TimeTypeDef *sTime);
+	struct tm* get_tm();
+	void set_tm(struct tm *tm2);
+	time_t get_timestamp();
+	void set_timestamp(time_t ts);
+	u32 get_cnt();
+	void set_cnt(u32 cnt);
+	void BKUPWrite(uint32_t BackupRegister, uint32_t Data);
+	u32 BKUPRead(uint32_t BackupRegister);
+};
+
+
+#endif /* STM32_INC_C_RTC_HPP_ */
---
+++ stm32/Src/c_rtc.cpp
@@ -0,0 +1,87 @@
+/*
+ * c_rtc.cpp
+ *
+ *  Created on: Jun 1, 2021
+ *      Author: xrj
+ */
+
+
+#include "c_rtc.hpp"
+
+struct tm tm1;
+
+extern uint32_t RTC_ReadTimeCounter(RTC_HandleTypeDef*);
+extern HAL_StatusTypeDef RTC_WriteTimeCounter(RTC_HandleTypeDef*, uint32_t);
+
+
+void C_RTC::HAL2tm(struct tm* tm2, RTC_DateTypeDef *sDate, RTC_TimeTypeDef *sTime)
+{
+	tm2->tm_year = (int)sDate->Year+100;
+	tm2->tm_mon = sDate->Month-1;
+	tm2->tm_mday = sDate->Date;
+	tm2->tm_wday = sDate->WeekDay;
+	tm2->tm_hour = sTime->Hours;
+	tm2->tm_min = sTime->Minutes;
+	tm2->tm_sec = sTime->Seconds;
+}
+
+void C_RTC::tm2HAL(struct tm* tm2, RTC_DateTypeDef *sDate, RTC_TimeTypeDef *sTime)
+{
+	sDate->Year = tm2->tm_year-100;
+	sDate->Month = tm2->tm_mon+1;
+	sDate->Date = tm2->tm_mday;
+	sDate->WeekDay = tm2->tm_wday;
+	sTime->Hours = tm2->tm_hour;
+	sTime->Minutes = tm2->tm_min;
+	sTime->Seconds = tm2->tm_sec;
+}
+
+struct tm* C_RTC::get_tm()
+{
+	RTC_TimeTypeDef sTime;
+	RTC_DateTypeDef sDate;
+	HAL_RTC_GetTime(this, &sTime, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(this, &sDate, RTC_FORMAT_BIN);
+	HAL2tm(&tm1, &sDate, &sTime);
+	return &tm1;
+}
+
+void C_RTC::set_tm(struct tm *tm2)
+{
+	RTC_TimeTypeDef sTime;
+	RTC_DateTypeDef sDate;
+	tm2HAL(tm2, &sDate, &sTime);
+	HAL_RTC_SetTime(this, &sTime, RTC_FORMAT_BIN);
+	HAL_RTC_SetDate(this, &sDate, RTC_FORMAT_BIN);
+}
+
+time_t C_RTC::get_timestamp()
+{
+	return mktime(get_tm());
+}
+
+void C_RTC::set_timestamp(time_t ts)
+{
+	struct tm *tm2 = localtime(&ts);
+	set_tm(tm2);
+}
+
+u32 C_RTC::get_cnt()
+{
+	  return RTC_ReadTimeCounter(this);
+}
+
+void C_RTC::set_cnt(u32 cnt)
+{
+	RTC_WriteTimeCounter(this, cnt);
+}
+
+void C_RTC::BKUPWrite(uint32_t BackupRegister, uint32_t Data)
+{
+	HAL_RTCEx_BKUPWrite(this, BackupRegister, Data);
+}
+
+u32 C_RTC::BKUPRead(uint32_t BackupRegister)
+{
+    return HAL_RTCEx_BKUPRead(this, BackupRegister);
+}
