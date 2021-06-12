f12f89195ab79f3599a8d02d4e506cd8a39f366f
Xu Ruijun <1687701765@qq.com>
2021-06-12 19:51:02 +0800

    feat: pins_manager struct, but unused

--- common/Inc/pins_manager.hpp
+++ common/Inc/pins_manager.hpp
@@ -4,6 +4,42 @@
 #include "x_base.hpp"
 #include "mylibs_config.hpp"
 
+
+#pragma pack(1)
+//Pin用途接口类型, 每种类型都难以共用同一个引脚
+typedef enum{
+	Pin_GPIO=0, //包含EXTI
+	Pin_UART,
+	Pin_SPI,    //不包含CS引脚
+	Pin_I2C,
+	Pin_SDIO,
+	Pin_USB,
+	Pin_TIM,
+	Pin_ADC,
+	Pin_OSC
+}PinFunc;
+
+//接口实现类型
+typedef enum{
+	PinFunc_Software=0,
+	PinFunc_Hardware
+}PinImpType;
+
+typedef struct{
+	unsigned int PinOut:1;
+	unsigned int PinIn:1;
+	unsigned int PinAna:1;
+	PinImpType imp:1;
+}PinBits;
+
+typedef struct{
+	PinFunc func:8;
+	PinBits bits:8;
+	unsigned int dev_id:4;
+	unsigned int pin_id:4;
+}PinSave;
+#pragma pack()
+
 typedef enum{
 	NoLock = 0,
 	LockAtFirstOn,
@@ -16,6 +52,7 @@ typedef enum{
 	InitCfg_Disable,
 }InitCfg;
 
+//TODO: PinCfg each connect, not each pin
 typedef struct{
 	C_Pin p8b;
 	bool keep:1;    //keep use flag in disable, note: will still load CfgDisable when GPIO_Conn entry disable state.
