bf023015e1b2583c80c75274b800d3210f6410ce
徐瑞骏 <1687701765@qq.com>
2022-07-23 10:51:03 +0800

    把ops.hpp的纯C语言部分分割成ops.h

--- common/others/Inc/ops.hpp
+++ common/others/Inc/ops.hpp
@@ -5,36 +5,13 @@
  *      Author: xrj
  */
 
-#ifndef __OPS_H__
-#define __OPS_H__
+#ifndef __OPS_HPP__
+#define __OPS_HPP__
 
 #include "mylibs_config.hpp"
 #include "myints.h"
+#include "ops.h"
 
-#if !defined(STM32F1) && !defined(STM32F4)
-
-#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
-#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(BIT))
-#define TOGG_BIT(REG, BIT)    ((REG) ^= (BIT))
-#define READ_BIT(REG, BIT)    ((REG) & (BIT))
-#define CLEAR_REG(REG)        ((REG) = (0x0))
-#define WRITE_REG(REG, VAL)   ((REG) = (VAL))
-#define READ_REG(REG)         ((REG))
-#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
-#define POSITION_VAL(VAL)     (__CLZ(__RBIT(VAL)))
-
-#endif
-
-//a,b,c const, x,y,z vars
-
-#define value_lower(x, a)   (x<a)?a:x
-#define value_upper(x, b)   (x>b)?b:x
-#define value_clip(x, a, b) ((x<a)?a:(x>b?b:x))
-
-#define mean(s, n)      (s/n)
-#define std(s, s2, n)   ((s2 - s1*s1/n)/n)
-
-#define CAST(t, x) (*(t*)(&(x)))
 
 template<typename T>
 u32 uint_sqrt(T x)
